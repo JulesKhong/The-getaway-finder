e5e8758e0869be92cd9b2ab49e348b1772a9f0bf
Julia Dickey <julia.dickey@gmail.com>
2016-06-03 10:09:45 -0700

    change alert results to copy that appears at the bottom in divs that appear

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-// debugger;
+
   $("#destination").submit(function(event) {
     event.preventDefault();
 
@@ -13,16 +13,24 @@ $(document).ready(function(){
     var qFour = parseInt($("#questionFour").val());
     var qFive = parseInt($("#questionFive").val());
     var result = sum(qOne, qTwo, qThree, qFour, qFive);
-    // alert(result);
       if (result < 17){
-        alert("you are a snowbird");
+        $("#snow").show();
       } else if (result < 25) {
-        alert("you are a beach bum")
+        $("#beach").show();
       } else if (result < 33) {
-        alert("you are a creative retreat")
+        $("#creative").show();
       } else  {
-        alert("you are a party animal!")
+        $("#party").show();
       }
-  // }
+
+      // if (result < 17){
+      //   alert("you are a snowbird");
+      // } else if (result < 25) {
+      //   alert("you are a beach bum")
+      // } else if (result < 33) {
+      //   alert("you are a creative retreat")
+      // } else  {
+      //   alert("you are a party animal!")
+      // }
   });
 });
