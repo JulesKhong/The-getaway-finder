9eff08b09b5e54b475c43070330f6bd45de85ba7
Julia Dickey <julia.dickey@gmail.com>
2016-06-03 10:01:48 -0700

    show all 5 questions with alert results that accurately show outcome.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-  var qOne = parseInt($("form#questionOne").val());
-  var qTwo = parseInt($("form#questionTwo").val());
-  var qThree = parseInt($("form#questionThree").val());
-  var qFour = parseInt($("form#questionFour").val());
-  var qFive = parseInt($("form#questionFive").val());
-  
-}
+// debugger;
+  $("#destination").submit(function(event) {
+    event.preventDefault();
+
+    var sum = function(qOne, qTwo, qThree, qFour, qFive){
+      return qOne + qTwo + qThree + qFour + qFive;
+    };
+
+    var qOne = parseInt($("#questionOne").val());
+    var qTwo = parseInt($("#questionTwo").val());
+    var qThree = parseInt($("#questionThree").val());
+    var qFour = parseInt($("#questionFour").val());
+    var qFive = parseInt($("#questionFive").val());
+    var result = sum(qOne, qTwo, qThree, qFour, qFive);
+    // alert(result);
+      if (result < 17){
+        alert("you are a snowbird");
+      } else if (result < 25) {
+        alert("you are a beach bum")
+      } else if (result < 33) {
+        alert("you are a creative retreat")
+      } else  {
+        alert("you are a party animal!")
+      }
+  // }
+  });
 });
